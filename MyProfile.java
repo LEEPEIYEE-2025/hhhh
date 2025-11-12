81295cce14734983317a0ee519426ee7db2d5640
LEEPY <25006494@siswa.um.edu.my>
2025-11-12 11:51:25 +0800

    test

---
+++ MyProfile.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l1q2;
+
+/**
+ *
+ * @author User
+ */
+public class MyProfile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String full_name = "Lee Pei Yee";
+        int matric_number = 25006494;
+        String address = "No15, Jalan Utama, Taman Desa Aulong, 34000, Taiping, Perak.";
+        String email = "25006494@siswa.um.edu.my";
+        long contact = 60134727343L;
+        
+        System.out.println("Full name: " + full_name);
+        System.out.println("Matric number: " + matric_number);
+        System.out.println("Address: " + address);
+        System.out.println("E-mail: " + email);
+        System.out.println("Contact: " + contact);
+    }
+    
+}
